2c73756ede06c165f50231a1b930df53ee0e0ce8
SanyamGarg <sanyam99garg@gmail.com>
2018-09-16 02:31:44 +0530

    second commit

--- Intellij_java/src/Game.java
+++ Intellij_java/src/Game.java
@@ -21,15 +21,20 @@ public class Game extends JFrame implements KeyListener {
     static int[] obsy = new int[6];
     static int[] obsx = new int[6];
     static int m;
-    static int h;
 
     static Graphics graphics;
 
     static Image bg1 = null;
     static Image handle = null;
+    static Image gameover = null;
+    static int score;
 
     static Image[] img = new Image[8];
-    static boolean b;
+    static boolean b=false;
+    static boolean bol=false;
+    static boolean first=true;
+
+    static int yVel=5;
 
 
     Game() {
@@ -50,7 +55,7 @@ public class Game extends JFrame implements KeyListener {
         f.setVisible(true);
         f.setResizable(false);
         f.setFocusable(true);
-        f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
         f.setLayout(null);
 
         graphics = p.getGraphics();
@@ -65,22 +70,16 @@ public class Game extends JFrame implements KeyListener {
             img[5] = ImageIO.read(Game.class.getClassLoader().getResource("images/pappu6.png"));
             img[6] = ImageIO.read(Game.class.getClassLoader().getResource("images/pappu7.png"));
             img[7] = ImageIO.read(Game.class.getClassLoader().getResource("images/pappu8.png"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        try {
             bg1 = ImageIO.read(Game.class.getClassLoader().getResource("images/gamebg.png"));
             handle = ImageIO.read(Game.class.getClassLoader().getResource("images/handle.png"));
+            gameover = ImageIO.read(Game.class.getClassLoader().getResource("images/gameover.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        int h = 0;
-        for (int g = 0; g < 3; g++) {
+        int h=0;
+        for (int g = 0; g < 2; g++) {
             obsx[g] = 1000 + h;
             h += 500;
             Random rd = new Random();
@@ -92,17 +91,30 @@ public class Game extends JFrame implements KeyListener {
         graphics.drawImage(bg1, bx1, by, null);
 
         int k;
-        while (true) {
+        while (first) {
+
             for (k = 0; k < 8; k++) {
                 graphics.drawImage(img[k], cx, cy, null);
                 try {
-                    Thread.sleep(80);
+                    Thread.sleep(5);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                graphics.clearRect(cx, cy, 60, 60);
+               // graphics.clearRect(cx, cy, 60, 60);
                 graphics.drawImage(bg1, bx1, by, null);
 
+                if(b){
+                    //System.out.println("main");
+                    cy+=yVel;
+                    obstacles();
+
+                    if(bol) {
+                        first = false;
+                        graphics.drawImage(gameover, 155, 0, null);
+                        System.out.println("Score:"+score);
+
+                    }
+                }
             }
         }
 
@@ -117,25 +129,28 @@ public class Game extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("this is keyPressed");
+
         int code = e.getKeyCode();
-        //  b=true;
+
         if (code == KeyEvent.VK_SPACE) {
-            int k = 8;
-            while (k > 0) {
-                cy = cy - k;
-                k = k - 2;
-                if (i == 8)
-                    i = 0;
+            b=true;
+            yVel=-5;
+            int k = 4;
+            while (k >= 0) {
+                cy = cy + yVel -k;
+                k = k - 1;
                 try {
                     Thread.sleep(45);
                 } catch (Exception y) {
                     y.printStackTrace();
                 }
+                i++;
+                if (i == 8)
+                    i = 0;
                 graphics.drawImage(img[i], cx, cy, null);
                 graphics.drawImage(bg1, bx1, bx2, null);
             }
-
+            yVel = 5;
         }
     }
 
@@ -144,66 +159,69 @@ public class Game extends JFrame implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("Key Released");
-        // b = false;
-        int code = e.getKeyCode();
-        if (code == KeyEvent.VK_SPACE) {
 
-            print();
-        }
     }
 
-    public void print() {
-        // int h = 0;
-        while (true) {
-            // cy =+h;
-            //   h= h+2;
-            // gravity();
+    public static void obstacles() {
+
             if (bx1 == -1000)
                 bx1 = 1000;
             if (bx2 == -1000)
                 bx2 = 1000;
-            bx1--;
-            bx2--;
+            bx1-=2;
+            bx2-=2;
             graphics.drawImage(bg1, bx1, by, null);
             graphics.drawImage(bg1, bx2, by, null);
 
-            graphics.drawImage(img[i], cx, cy, null);
 
-
-
-            if(m==6)
-                m=0;
-
-            int g;
             Random rd = new Random();
 
-            for(g=0;g<2;g++) {
-                if (obsx[g] == 0) {
+            for(int g=0;g<2;g++) {
+
+                if (obsx[g] <= 0) {
+                    score++;
                     obsx[g] = 1000;
                     obsy[g] = rd.nextInt(313) + 188;
                 }
-                obsx[g]--;
+                obsx[g]-=4;
             }
 
             for(m=0;m<2;m++){
-                graphics.drawImage(handle, obsx[m], obsy[m], null);
-                if(obsy[m]<425)
-                    graphics.drawImage(handle, obsx[m], obsy[m] - 425, null);
-                else
-                    graphics.drawImage(handle, obsx[m],0 , null);
 
+               if(obsy[m]<450)
+                graphics.drawImage(handle, obsx[m], obsy[m], null);
+                //System.out.println("obstacle");
+                if(obsy[m]<420) {
+                    graphics.drawImage(handle, obsx[m], obsy[m] - 452, null);
+                    check(obsy[m] - 140);
+                }
+                else {
+                    graphics.drawImage(handle, obsx[m], 0, null);
+                    check(312);
+                }
             }
+
+    }
+
+    static void check(int ul){
+        int r;
+        if(cy < -50 || cy > 550)
+            bol = true;
+        for(r=0;r<4;r++) {
+            if (cx + 30 + r == obsx[m] && cy <= ul)
+                bol = true;
+            if(bol)
+                break;
+
+
+            if (cx + 30 + r == obsx[m] && cy + 60 >= obsy[m])
+                if (obsy[m] < 450)
+                    bol = true;
+                if(bol)
+                    break;
 
         }
     }
-    /*public void gravity(){
-            if(b){
-                h=0;
-            }
-            cx=cx+h;
-            h += 2;
-    }*/
 
 
 }
